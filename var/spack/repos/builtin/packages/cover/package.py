8e130f7f0fc1c19926236e41783105f064f642e1
Martin Aumüller <aumuell@reserv.at>
2020-11-19 20:05:52 +0100

    cover: disable implicit cmake package deps

--- var/spack/repos/builtin/packages/cover/package.py
+++ var/spack/repos/builtin/packages/cover/package.py
@@ -5,6 +5,7 @@
 
 from spack import *
 
+from sys import platform
 
 class Cover(CMakePackage):
     """Collaborative Visualization and Simulation Environment"""
@@ -18,53 +19,138 @@ class Cover(CMakePackage):
     # version('1.2.3', '0123456789abcdef0123456789abcdef')
     version('develop', branch='master', submodules=True)
 
+    variant('x11', default=not platform=='darwin', description='Use X11 Window system')
+    variant('mpi', default=False, description='Enable MPI support - required for Vistle')
+    variant('embree', default=False, description='Interactive spray simulation')
+    variant('virvo', default=True, description='Enable volume rendering')
+    variant('visionaray', default=False, description='Enable interactive ray-tracing')
+
     depends_on('cmake@3.3:', type='build')
 
     depends_on('python@2.7:', type=('build', 'run'))
 
     depends_on('flex', type='build')
     depends_on('bison', type='build')
-    depends_on('swig', type='build')
+    #depends_on('swig', type='build')
 
     depends_on('xerces-c')
     depends_on('curl')
     depends_on('qt+opengl')
     depends_on('glu')
     depends_on('glew')
-    depends_on('mpi')
-    depends_on('boost+pic')
-    depends_on('cfitsio')
-
-    depends_on('tbb')
-
-    depends_on('ffmpeg')
-    depends_on('embree')
-    depends_on('libtiff')
-    depends_on('libpng')
-    depends_on('zlib')
-    depends_on('libjpeg-turbo')
-
-    #depends_on('openssl')
-    depends_on('speex')
-
-    # depends_on('vtk', when='+vtk')
-
-    # depends_on('assimp', when='+assimp')
-
-    # depends_on('openscenegraph', when='+osg')
     depends_on('openscenegraph')
+    depends_on('libx11', when='+x11')
+
+    depends_on('mpi', when='+mpi')
+
+    depends_on('boost')
+    #depends_on('tbb', when='+virvo')
+
+    #depends_on('cfitsio')
+
+    #depends_on('ffmpeg')
+    depends_on('embree', when='+embree')
+
+    depends_on('zlib')
+    depends_on('libpng')
+    depends_on('libtiff')
+
+    #depends_on('libjpeg-turbo')
+
+    #depends_on('speex')
+
+    package_spec = {
+        'cfitsio': 'cfitsio',
+        'Boost': 'boost',
+        'OpenSSL': 'openssl',
+        'Git': 'git',
+        'VTK': 'vtk',
+        'SWIG': 'swig',
+        'JPEGTURBO': 'libjpeg-turbo',
+        'JPEG': 'jpeg',
+        'FFMPEG': 'ffmpeg',
+        'Snappy': 'snappy',
+        'TBB': 'tbb',
+        'MPI': 'mpi',
+        'FLEX': 'flex',
+        'BISON': 'bison',
+        'embree': 'embree',
+        'SPEEX': 'speex',
+        'PROJ4': 'proj',
+        'SDL': 'sdl2',
+        'EIGEN': 'eigen',
+        'GDAL': 'gdal',
+        'GeoTIFF': 'libgeotiff',
+        'VTKm': 'vtk-m',
+        'Freeimage': 'freeimage',
+        'BLAS': 'blas',
+        'LAPACK': 'lapack',
+        'Cg': None,
+        'HIDAPI': None,
+        'Alut': None,
+        'OpenVR': None,
+        'PCL': None,
+        'PythonLibs': 'python',
+        'LibUSB1': 'libusb',
+        'X11': 'libx11',
+        'OpenCV': 'opencv',
+        'GLUT': 'freeglut',
+        'LibVncServer': None,
+        'NVML': None,
+        'VRPN': None,
+        'Steereo': None,
+        'FMOD': None,
+        'WiringPi': None,
+        'ARToolKit': None,
+        'NATNET': None,
+        'ZSPACE': None,
+        'OSGCAL': None,
+        'OsgQt': None,
+        'OsgEarth': None,
+        'Faro': None,
+        'JT': None,
+        'SISL': None,
+        'E57': None,
+        'OVR': None,
+        'OsgEphemeris': None,
+        'SLAM6D': None,
+        'WiiYourself': None,
+        'PHYSX': None,
+        'CUDPP': None,
+        'JSBSim': None,
+        'OpenCRG': None,
+        'LAMURE': None,
+        'Schism': None,
+        'MidiFile': None,
+        'OSC': None,
+        'WMFSDK': None,
+        'Surface': None,
+        #'PTHREAD': None,
+        'OpenEXR': 'openexr',
+        'CUDA': 'cuda',
+        'PbrtParser': None,
+    }
 
     def cmake_args(self):
         """Populate cmake arguments for COVISE."""
 
-        args = []
+        spec = self.spec
+
+        args = [
+            self.define_from_variant('COVISE_USE_VIRVO', 'virvo'),
+            self.define_from_variant('COVISE_USE_VISIONARAY', 'visionaray'),
+            self.define_from_variant('COVISE_USE_X11', 'x11'),
+        ]
 
         args.append('-DCOVISE_WARNING_IS_ERROR=OFF')
         args.append('-DCOVISE_BUILD_ONLY_COVER=ON')
-        args.append('-DCOVISE_USE_VISIONARAY=OFF')
-        args.append('-DCOVISE_USE_VIRVO=ON')
-        args.append('-DCOVISE_USE_CUDA:BOOL=OFF')
+
         args.append('-DCOVISE_CPU_ARCH:STRING=')
         args.append('-DARCHSUFFIX:STRING=spack')
+
+        for package in self.package_spec:
+            s = self.package_spec[package]
+            if not s or not spec.satisfies('^'+s):
+                args.append('-DCMAKE_DISABLE_FIND_PACKAGE_'+package+'=TRUE')
 
         return args
