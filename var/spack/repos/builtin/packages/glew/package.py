b9b51bf56993153b6344443e9d137d96eab25415
Martin Aumüller <aumuell@reserv.at>
2020-11-20 09:05:38 +0100

    glew: change download url for 2.2.0

--- var/spack/repos/builtin/packages/glew/package.py
+++ var/spack/repos/builtin/packages/glew/package.py
@@ -8,8 +8,9 @@ class Glew(CMakePackage):
     """The OpenGL Extension Wrangler Library."""
 
     homepage = "http://glew.sourceforge.net/"
-    url      = "https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.tgz"
+    url      = "https://downloads.sourceforge.net/project/glew/glew/2.2.0/glew-2.2.0.tgz"
 
+    version('2.2.0',  sha256='d4fc82893cfb00109578d0a1a2337fb8ca335b3ceccf97b97e5cc7f08e4353e1')
     version('2.1.0',  sha256='04de91e7e6763039bc11940095cd9c7f880baba82196a7765f727ac05a993c95')
     version('2.0.0',  sha256='c572c30a4e64689c342ba1624130ac98936d7af90c3103f9ce12b8a0c5736764')
 
@@ -21,6 +22,7 @@ class Glew(CMakePackage):
 
     @run_after('cmake')
     def patch_glew_pc(self):
-        # https://github.com/Homebrew/legacy-homebrew/issues/22025
-        # Note: This file is generated only after cmake is run
-        filter_file(r'Requires: glu', '', 'glew.pc')
+        if self.spec.satisfies('@:2.1.99'):
+            # https://github.com/Homebrew/legacy-homebrew/issues/22025
+            # Note: This file is generated only after cmake is run
+            filter_file(r'Requires: glu', '', 'glew.pc')
