bc51b7774cd4aa36236ed6bfa2b9a8e42ad3fa83
Martin Aumüller <aumuell@reserv.at>
2023-07-25 11:25:12 +0200

    libheif: checksum new versions

--- var/spack/repos/builtin/packages/libheif/package.py
+++ var/spack/repos/builtin/packages/libheif/package.py
@@ -12,6 +12,10 @@ class Libheif(CMakePackage):
     homepage = "https://github.com/strukturag/libheif"
     url = "https://github.com/strukturag/libheif/archive/refs/tags/v1.12.0.tar.gz"
 
+    version("1.16.2", sha256="d207f2ff5c86e6af3621c237f186130b985b7a9ff657875944b58ac5d27ba71c")
+    version("1.15.2", sha256="30a2736ae0247389aaa43ec70357221500c49a68db39fda94da8d5bdc786fe3b")
+    version("1.14.2", sha256="e9c88e75e3b7fad9df32e42d28646752de2679df57efddfb3f63cd25110ce9d9")
+    version("1.13.0", sha256="50def171af4bc8991211d6027f3cee4200a86bbe60fddb537799205bf216ddca")
     version("1.12.0", sha256="086145b0d990182a033b0011caadb1b642da84f39ab83aa66d005610650b3c65")
 
     depends_on("cmake@3.13:", type="build")
