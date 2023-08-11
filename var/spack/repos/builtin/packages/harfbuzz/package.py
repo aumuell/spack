5d232112deebc925f04da0070d0473726eb4bc05
Martin Aumüller <aumuell@reserv.at>
2023-08-11 14:38:13 +0200

    harfbuzz: new versions

--- var/spack/repos/builtin/packages/harfbuzz/package.py
+++ var/spack/repos/builtin/packages/harfbuzz/package.py
@@ -18,6 +18,8 @@ class Harfbuzz(MesonPackage, AutotoolsPackage):
         conditional("autotools", when="@:2.9"), conditional("meson", when="@3:"), default="meson"
     )
 
+    version("8.1.1", sha256="0305ad702e11906a5fc0c1ba11c270b7f64a8f5390d676aacfd71db129d6565f")
+    version("8.0.1", sha256="c1ce780acd385569f25b9a29603d1d5bc71e6940e55bfdd4f7266fad50e42620")
     version("7.3.0", sha256="20770789749ac9ba846df33983dbda22db836c70d9f5d050cb9aa5347094a8fb")
     version("7.2.0", sha256="fc5560c807eae0efd5f95b5aa4c65800c7a8eed6642008a6b1e7e3ffff7873cc")
     version("6.0.0", sha256="1d1010a1751d076d5291e433c138502a794d679a7498d1268ee21e2d4a140eb4")
